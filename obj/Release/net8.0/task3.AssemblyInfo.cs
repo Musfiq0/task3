f4130b576241b0dc5f5edc321c26a617c8113da0
Musfiq0 <rahmanmusfiqur414@gmail.com>
2025-10-02 12:25:33 +0600

    latest

--- obj/Release/net8.0/task3.AssemblyInfo.cs
+++ obj/Release/net8.0/task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1df4ef83fa7f2fa303dd4155442d3addc1c3c5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07f872129d3f7edde647fa1f469efec959846f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
